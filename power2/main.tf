7bc852de9804ca7c73fe2a92fcc7cacf33a55927
jiewang <jiewang@tencent.com>
2025-03-05 15:31:31 +0800

    update tf

---
+++ power2/main.tf
@@ -0,0 +1,33 @@
+# 定义 Terraform 的版本
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# 定义一个 null_resource
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+}
+
+resource "null_resource" "test2" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+}
+
+resource "null_resource" "test3" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+}
+
+# 输出 null_resource 的 ID
+output "null_resource_id" {
+  value = null_resource.test.id
+}
