54d45bcb64e2391a6061fd8ea071475708ba9266
jiewang <jiewang@tencent.com>
2025-03-05 15:20:52 +0800

    update tf

--- main.tf
+++ main.tf
@@ -15,15 +15,18 @@ resource "null_resource" "test" {
   }
 }
 
-# # 定义第二个 null_resource
-# resource "null_resource" "test_2" {
-#   provisioner "local-exec" {
-#     command = "echo 'Hello from the second null_resource!'"
-#   }
+resource "null_resource" "test2" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+}
 
-#   # 依赖于第一个 null_resource
-#   depends_on = [null_resource.test_1]
-# }
+resource "null_resource" "test3" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+}
+
 # 输出 null_resource 的 ID
 output "null_resource_id" {
   value = null_resource.test.id
