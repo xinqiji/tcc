49e69effcba113a25388673459456a9dfd639f16
辛弃疾 <xinqiji@me.com>
2025-03-05 14:43:59 +0800

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,12 +1,21 @@
-resource "tencentcloud_vpc" "jiewang-network" {
-  cidr_block   = "10.18.32.0/20"
-  is_multicast = "false"
-  name         = "jiewang-network"
+# 定义 Terraform 的版本
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
 }
 
+# 定义一个 null_resource
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+}
 
-resource "tencentcloud_vpc" "jiewang-network" {
-  cidr_block   = "10.18.43.0/20"
-  is_multicast = "false"
-  name         = "jiewang-network"
+# 输出 null_resource 的 ID
+output "null_resource_id" {
+  value = null_resource.test.id
 }
