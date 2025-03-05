466ec525cd0aea83a1a8e2d1755d045bd897687e
jiewang <jiewang@tencent.com>
2025-03-05 14:55:48 +0800

    update tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,15 +15,15 @@ resource "null_resource" "test" {
   }
 }
 
-# 定义第二个 null_resource
-resource "null_resource" "test_2" {
-  provisioner "local-exec" {
-    command = "echo 'Hello from the second null_resource!'"
-  }
+# # 定义第二个 null_resource
+# resource "null_resource" "test_2" {
+#   provisioner "local-exec" {
+#     command = "echo 'Hello from the second null_resource!'"
+#   }
 
-  # 依赖于第一个 null_resource
-  depends_on = [null_resource.test_1]
-}
+#   # 依赖于第一个 null_resource
+#   depends_on = [null_resource.test_1]
+# }
 # 输出 null_resource 的 ID
 output "null_resource_id" {
   value = null_resource.test.id
