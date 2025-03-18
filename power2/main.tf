f63d177a384af87d43f89151aad73dde889a3b72
jiewang <jiewang@tencent.com>
2025-03-18 16:00:26 +0800

    update gitops_chart

--- power2/main.tf
+++ power2/main.tf
@@ -34,7 +34,7 @@ resource "null_resource" "test7" {
 }
 
 
-resource "null_resource" "test6" {
+resource "null_resource" "test7" {
   provisioner "local-exec" {
     command = "echo 'Hello, Terraform!'"
   }
